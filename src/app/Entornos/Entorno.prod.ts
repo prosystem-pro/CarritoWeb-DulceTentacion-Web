bddedc720df18d0a4dd87de71dbb433fc893140e
prosystem-pro <prosystem490@gmail.com>
2025-07-24 11:49:39 -0600

    Cambio a railway

--- src/app/Entornos/Entorno.prod.ts
+++ src/app/Entornos/Entorno.prod.ts
@@ -1,6 +1,6 @@
 export const Entorno = {
   Produccion: true,
-  ApiUrl: 'https://carritoweb-dulcetentacion-api.onrender.com/api/',
+  ApiUrl: '  https://carritoweb-dulcetentacion-api-production.up.railway.app/api/',
   // ApiUrl: 'https://carritoweb-promesadios-api-production.up.railway.app/api/',
   NombreEmpresa: 'DulceTentacion'
 };
